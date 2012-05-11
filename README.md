3421b400c3ac4070279f27b71c0ba1d705a5c240
Adrianus Kleemans <a.kleemans@gmail.com>
2012-05-12 01:34:19 +0200

    Initial commit onto GitHub. Added script and readme.

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-latex-wordcount
-===============
+# latex-wordcount
 
+## Credit / License
+Basic part of the script is from Stuart Curtis - [latexwordcount](http://sourceforge.net/projects/latexwordcount/).
+
+It is therefore under the same license:
+
+	This code is released under the GPLv2 license allowing its free use, modification and distribution.
+
+See the [license file](raw/master/gpl-2.0.txt) for more information.
+
+## Purpose
 Counts number of words in a LaTeX file.
+
+Just run the script with a *tex*-file as argument:
+
+    wordcount.py file.tex
+
+## Word count
+Counted as text:
+
+* normal text
+* headers
+
+Not counted as words:
+
+* references (*\ref*, *\cite*)
+* header, footer
+
+
+## Know Bugs
+* Doesn't count word if adjacent to backslash (*word\normalsize* is not counted)
+* Bold, italic and underlined words are not counted (*\textbf{word}* is not counted)
+* Counts commands that begin '{\' as words
+* Raises error if $ and $$ occur in a single word
---
+++ wordcount.py
@@ -0,0 +1,142 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+#
+# Counts number of words in a LaTeX file.
+#
+# Some changes by Adrianus Kleemans
+# modified 11.05.2012
+# Original script by Stuart Curtis
+#
+
+import os
+import sys
+
+# read command line argument
+try:
+    path = sys.argv[1]
+except IndexError:
+    print("Please provide a file name for counting.")
+    sys.exit()
+
+# read file
+try:
+    f = open(path, 'rb')
+except IOError:
+    print("Please provide a valid .tex-file.")
+    sys.exit()
+
+wholewords = f.read()
+f.close()
+
+# start main script
+lines = wholewords.splitlines()
+
+# remove comments
+i = 0
+while i < len(lines):
+        if len(lines[i]) > 0 and lines[i][0] == "%":
+                del(lines[i])
+                continue
+        if lines[i].find("%") != -1:
+                if lines[i][lines[i].find("%")-1] != ("\\"):
+                        lines[i] = lines[i][:lines[i].find("%")]
+        if len(lines[i]) == 0:
+                lines.pop(i)
+                continue
+        i +=1
+                        
+words = []
+for i in range(len(lines)):
+        words.append(lines[i].split())
+
+words = sum(words, [])
+                
+ignoredList = ["document", "tiny", "scriptsize", "footnotesize", "small", "normalsize", "large", "Large", "LARGE", "huge", "huge", "flushleft", "center", "flushright"]
+
+global wordNos
+
+wordNos = range(len(words))
+
+#########################################
+
+def begin(command, index):
+        i = index
+        if ignoredList.count(command):
+                try: wordNos.remove(i)
+                except ValueError:
+                                return
+                        
+                while words[i].find("\\end{" + str(command) +"}") == -1:
+                        i+=1
+                try: wordNos.remove(i)
+                except ValueError:
+                        return
+        else:
+                while words[i].find("\\end{" + str(command) +"}") == -1:
+                        try: wordNos.remove(i)
+                        except ValueError:
+                                return
+                        i+=1
+                wordNos.remove(i)
+                return
+                
+#########################################
+
+def backslashFn(index, opnCu, clsCu, opnSq, clsSq):
+        try: wordNos.remove(index)
+        except ValueError:
+                return
+        opnCu += words[index].count("{")
+        clsCu += words[index].count("}")
+        opnSq += words[index].count("[")
+        clsSq += words[index].count("]")
+        if clsCu == opnCu and clsSq == opnSq:
+                return
+        else:
+                return backslashFn(index+1, opnCu, clsCu, opnSq, clsSq)
+                
+#########################################
+
+def dollar(index, no, sinDbl):
+        #print index, no
+        try: wordNos.remove(index)
+        except ValueError:
+                return
+        no += words[index].count("$"*sinDbl)
+        if no%2 == 0:
+                return
+        else:
+                return dollar(index+1, no, sinDbl)
+
+#########################################
+
+for i in range(len(words)):
+# "dollar sign" equations
+
+        if words[i].count("$")>0 and words[i].count("$")%2 == 0 and words[i].count("$$")%2 == 0:
+                try: wordNos.remove(i)
+                except ValueError:
+                        continue                
+        elif words[i].count("$")%2 == 1 and words[i].count("$$")%2 == 0 and wordNos.count(i) == 1:
+                wordNos.remove(i)
+                dollar(i+1, words[i].count("$"), 1)
+        elif words[i].count("$")%2 == 0 and words[i].count("$$")%2 == 1 and wordNos.count(i) == 1:
+                wordNos.remove(i)
+                dollar(i+1, words[i].count("$$"), 2)
+#begin and end functions
+        elif words[i].find("\\begin{") == 0:
+                begin(words[i][7:words[i].find("}")], i)
+        elif words[i].find("\\begin{") > 0:
+                begin(words[i][7:words[i].find("}")], i+1)
+#named functions
+        elif words[i].find("\\") == 0 and words[i].count("{") == words[i].count("}") and words[i].count("[") == words[i].count("]"):
+                try: wordNos.remove(i)
+                except ValueError:
+                        continue
+        elif words[i].find("\\") == 0 and words[i].count("{") > words[i].count("}") or words[i].count("[") > words[i].count("]"):
+                try: wordNos.remove(i)
+                except ValueError:
+                        continue
+                backslashFn(i+1, words[i].count("{"), words[i].count("}"), words[i].count("["), words[i].count("]"))
+
+print("{} words".format(len(wordNos)))
